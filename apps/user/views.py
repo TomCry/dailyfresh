3390a1f38c5cfc9d535c8bba3cba22d973b494d8
enzyme <zycctom@outlook.com>
2019-05-16 21:15:55 +0800

    05-12 个人中心历史记录改为动态显示 1、在view.py对应类和方法中，首先获取商品信息 2、修改前端页面，使用for循环 3、此处使用redis的商品id来遍历商品比较复杂，好好思考下

--- apps/user/views.py
+++ apps/user/views.py
@@ -5,10 +5,12 @@ from django.views.generic import View
 from django.http import HttpResponse
 from django.conf import settings
 from apps.user.models import User, Address
+from apps.goods.models import GoodsSKU
 from celery_tasks.tasks import send_register_active_email
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired
 from utils.minxi import LoginRequiredMixin
+from django_redis import get_redis_connection
 
 import re
 
@@ -195,6 +197,33 @@ class UserInfoView(LoginRequiredMixin, View):
         user = request.user
         address = Address.objects.get_default_address(user)
         # 获取用户的个人历史浏览记录
+        # from redis import StrictRedis
+        # sr = StrictRedis(host='192.168.33.239', port='6379', db=9)
+        con = get_redis_connection('default')
+        history_key = 'history_%d' % user.id
+        # 获取用户最新浏览的5个商品的id
+        sku_ids = con.lrange(history_key, 0, 4)
+
+        # 从数据库中查询用户浏览商品的具体信息
+        # goods_li = GoodsSKU.objects.filter(id__in=sku_ids)
+
+        # goods_res = []
+        # for a_id in sku_ids:
+        #     for goods in goods_li:
+        #         if a_id == goods.id:
+        #             goods_res.append(goods)
+        # 遍历获取用户浏览的历史商品信息
+        goods_li = []
+        for id in sku_ids:
+            goods = GoodsSKU.objects.get(id=id)
+            goods_li.append(goods)
+
+        # 组织上下文
+        context = {
+            'page': 'user',
+            'address': address,
+            'goods_li': goods_li
+        }
 
         return render(request, 'user_center_info.html', {'page': 'user', 'address': address})
 
